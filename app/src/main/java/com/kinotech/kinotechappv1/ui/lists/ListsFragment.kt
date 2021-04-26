c72dcfccf1d428297100c744e3d875fa8536b488
ananasann <paramonovaann39@gmail.com>
2021-04-26 21:49:56 +0300

    RecyclerAdapterLists, item_list, local list, update build.gradle, class ListOfMovie

--- app/src/main/java/com/kinotech/kinotechappv1/ui/lists/ListsFragment.kt
+++ app/src/main/java/com/kinotech/kinotechappv1/ui/lists/ListsFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -43,15 +41,18 @@ class ListsFragment : Fragment() {
     override fun onResume() {
         super.onResume()
 
-        fun generateListOfMovie(): List<ListsOfMovie>{
+        fun generateListOfMovie(): List<ListsOfMovie> {
             return listOf(
                 ListsOfMovie(
-                "Создать список","сорс","https://cdn25.img.ria.ru/images/156087/28/1560872802_0:778:1536:1642_600x0_80_0_0_606c2d47b6d37951adc9eaf750de22f0.jpg"
-            ), ListsOfMovie(
+                    "Создать список", "сорс", "https://cdn25.img.ria.ru/images/156087/28/1560872802_0:778:1536:1642_600x0_80_0_0_606c2d47b6d37951adc9eaf750de22f0.jpg"
+                ),
+                ListsOfMovie(
                     "Понравились", "5 фильмов", "https://cdn25.img.ria.ru/images/156087/28/1560872802_0:778:1536:1642_600x0_80_0_0_606c2d47b6d37951adc9eaf750de22f0.jpg"
-            ), ListsOfMovie(
+                ),
+                ListsOfMovie(
                     "Какое-то название", "7 фильмов", "https://cdn25.img.ria.ru/images/156087/28/1560872802_0:778:1536:1642_600x0_80_0_0_606c2d47b6d37951adc9eaf750de22f0.jpg"
-                ), ListsOfMovie(
+                ),
+                ListsOfMovie(
                     "Хорошо похотать", "10 фильмов", "https://cdn25.img.ria.ru/images/156087/28/1560872802_0:778:1536:1642_600x0_80_0_0_606c2d47b6d37951adc9eaf750de22f0.jpg"
                 )
             )
@@ -65,7 +66,6 @@ class ListsFragment : Fragment() {
             recyclerAdapter = RecyclerAdapterLists(normalnyContext)
             recyclerView.layoutManager = LinearLayoutManager(context)
             recyclerView.adapter = recyclerAdapter
-
         }
         recyclerAdapter.setMovieListItems(generateListOfMovie())
     }
--- app/src/main/java/com/kinotech/kinotechappv1/ui/lists/RecyclerAdapterLists.kt
+++ app/src/main/java/com/kinotech/kinotechappv1/ui/lists/RecyclerAdapterLists.kt
@@ -7,20 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.kinotech.kinotechappv1.R
 
-public class RecyclerAdapterLists (val context: Context) : RecyclerView.Adapter<RecyclerAdapterLists.MyViewHolder>(){
+public class RecyclerAdapterLists(val context: Context) : RecyclerView.Adapter<RecyclerAdapterLists.MyViewHolder>() {
 
-    var listsOfMovie : List<ListsOfMovie> = listOf()
-
+    var listsOfMovie: List<ListsOfMovie> = listOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
         return MyViewHolder(view)
     }
 
@@ -32,39 +30,37 @@ public class RecyclerAdapterLists (val context: Context) : RecyclerView.Adapter<
         val lists = listsOfMovie[position]
         holder.bind(lists)
 
-        //holder.imgList.setImageResource(listsOfMovie.)
+        // holder.imgList.setImageResource(listsOfMovie.)
        /* Glide.with(context).load(listsOfMovie.get(position).imgList)
             .apply(RequestOptions().centerCrop())
             .into(holder.imgList)*/
     }
 
-    fun setMovieListItems(movieList: List<ListsOfMovie>){
-        this.listsOfMovie = movieList;
+    fun setMovieListItems(movieList: List<ListsOfMovie>) {
+        this.listsOfMovie = movieList
         notifyDataSetChanged()
     }
 
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        fun bind(lists: ListsOfMovie){
+        fun bind(lists: ListsOfMovie) {
             val itemTitle: TextView = itemView.findViewById(R.id.item_title)
             val filmCount: TextView = itemView.findViewById(R.id.film_count)
             val imgListH: ImageView = itemView.findViewById(R.id.img_list)
 
             itemTitle.text = lists.itemTitle
             filmCount.text = lists.filmCount
-            val imgList:String = lists.imgList
-            Log.d("tag","karkar" + imgList)
+            val imgList: String = lists.imgList
+            Log.d("tag", "karkar" + imgList)
             var requestOptions = RequestOptions()
             requestOptions = requestOptions.transform(RoundedCorners(180))
             Glide
-                //.with(itemView.context)
+                // .with(itemView.context)
                 .with(itemView.context)
                 .load(imgList)
-                //.apply(requestOptions)
+                // .apply(requestOptions)
                 .error(R.drawable.ic_home_black_24dp)
                 .into(imgListH)
-
         }
-
     }
 }
