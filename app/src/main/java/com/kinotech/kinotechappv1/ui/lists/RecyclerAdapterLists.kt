af91e0e36a086b4785f055e0dc2aff868cb680ad
ananasann <paramonovaann39@gmail.com>
2021-04-26 22:41:21 +0300

    RecyclerAdapterLists, item_list, local list, update build.gradle, class ListOfMovie

--- app/src/main/java/com/kinotech/kinotechappv1/ui/lists/RecyclerAdapterLists.kt
+++ app/src/main/java/com/kinotech/kinotechappv1/ui/lists/RecyclerAdapterLists.kt
@@ -9,8 +9,6 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.RequestOptions
 import com.kinotech.kinotechappv1.R
 
 public class RecyclerAdapterLists(val context: Context) :
