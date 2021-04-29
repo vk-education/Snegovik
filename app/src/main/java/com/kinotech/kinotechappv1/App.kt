2beb2b7680a6a743ea39d6691f205bcb975b27b5
Ulyana Mayakova <u_mayakova@mail.ru>
2021-04-29 15:02:40 +0300

    List menu layout, film page layout, flow checks

---
+++ app/src/main/java/com/kinotech/kinotechappv1/App.kt
@@ -0,0 +1,31 @@
+package com.kinotech.kinotechappv1
+
+import android.app.Application
+import android.os.StrictMode
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build()
+            )
+
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build()
+            )
+        }
+    }
+}
