7604986af48bcc839333828e54a0b5a943d05d9e
Ulyana Mayakova <u_mayakova@mail.ru>
2021-05-27 19:46:37 +0300

    Español translation added

--- app/src/main/java/com/kinotech/kinotechappv1/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/kinotech/kinotechappv1/ui/profile/ProfileFragment.kt
@@ -6,9 +6,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -21,12 +18,13 @@ import com.kinotech.kinotechappv1.AuthActivity
 import com.kinotech.kinotechappv1.R
 import com.kinotech.kinotechappv1.databinding.FragmentProfileBinding
 import com.kinotech.kinotechappv1.ui.profile.subs.SubsFragment
+import com.kinotech.kinotechappv1.ui.profile.subs.SubscriptionsFragment
 
 class ProfileFragment : Fragment() {
 
-    private lateinit var photoAcc: ImageView
-    private lateinit var nickName: TextView
-    private lateinit var signOut: Button
+    //    private lateinit var photoAcc: ImageView
+//    private lateinit var nickName: TextView
+//    private lateinit var signOut: Button
     private lateinit var mSignInClient: GoogleSignInClient
     private lateinit var profileViewModel: ProfileViewModel
     private lateinit var model: ProfileSharedViewModel
@@ -36,20 +34,21 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        profileViewModel =
-            ViewModelProvider(this).get(ProfileViewModel::class.java)
-        
-        val root = inflater.inflate(R.layout.fragment_profile, container, false)
-        signOut = root.findViewById(R.id.imageExit)
-        nickName = root.findViewById(R.id.textProfile)
-        photoAcc = root.findViewById(R.id.photo)
+    ): View {
+        profileViewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+//
+//        val root = inflater.inflate(R.layout.fragment_profile, container, false)
+//        signOut = root.findViewById(R.id.image_exit)
+//        nickName = root.findViewById(R.id.textProfile)
+//        photoAcc = root.findViewById(R.id.photo)
 //        val picture_ctx = GoogleSignIn.getLastSignedInAccount(context)
 //        val picture = picture_ctx?.photoUrl
-        val button = root.findViewById<Button>(R.id.changeProfileButton)
-        button.setOnClickListener {
+//        val button = root.findViewById<Button>(R.id.changeProfileButton)
+        binding.changeProfileButton.setOnClickListener {
             loadfragment()
-//            var displayMessage = arguments?.getString("message")
+            var displayMessage = arguments?.getString("message")
             //root.findViewById<Button>(R.id.textProfile).text = displayMessage
         }
 
@@ -73,21 +72,20 @@ class ProfileFragment : Fragment() {
 //        val change = inflater.inflate(R.layout.change_profile, container, false)
 //        photoAcc = change.findViewById(R.id.change_photo)
 
-        return root
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        Log.d("fragmentollo", "onViewCreated")
-        super.onViewCreated(view, savedInstanceState)
-//        model = ViewModelProvider(requireActivity()).get(ProfileSharedViewModel::class.java)
-//        if (binding.photo.drawable != null) {
-//            model.putPhoto(binding.photo.drawable.toString().toUri())
-////        model.putPhoto(binding.textProfile.text.toString().toUri())
-//        }
-//            model.getPhoto().observe(viewLifecycleOwner, {
-//                binding.photo.setImageURI(it)
-//            })
-    }
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+////        model = ViewModelProvider(requireActivity()).get(ProfileSharedViewModel::class.java)
+////        if (binding.photo.drawable != null) {
+////            model.putPhoto(binding.photo.drawable.toString().toUri())
+//////        model.putPhoto(binding.textProfile.text.toString().toUri())
+////        }
+////            model.getPhoto().observe(viewLifecycleOwner, {
+////                binding.photo.setImageURI(it)
+////            })
+//    }
 
     private fun loadfragment() {
         val transaction = activity?.supportFragmentManager?.beginTransaction()
@@ -125,7 +123,7 @@ class ProfileFragment : Fragment() {
                 .requestEmail()
                 .build()
         mSignInClient = context?.let { GoogleSignIn.getClient(it, gso) }!!
-        signOut.setOnClickListener {
+        binding.imageExit.setOnClickListener {
             mSignInClient.signOut()
             val intent = Intent(context, AuthActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
@@ -137,12 +135,12 @@ class ProfileFragment : Fragment() {
         if (acc == null) {
             Log.d("check", "null")
         } else {
-            nickName.text = acc.displayName
+            binding.textProfile.text = acc.displayName
             Glide
                 .with(this)
                 .load(acc.photoUrl)
                 .error(R.drawable.ic_like_40dp)
-                .into(photoAcc)
+                .into(binding.photo)
         }
     }
 }
