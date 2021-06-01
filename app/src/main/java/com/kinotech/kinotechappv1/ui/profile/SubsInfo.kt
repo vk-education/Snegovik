93d710ce1101242f401259cdb31b8b25fa2d6697
Ulyana Mayakova <u_mayakova@mail.ru>
2021-06-01 17:57:11 +0300

    Subsinfo

--- app/src/main/java/com/kinotech/kinotechappv1/ui/profile/SubsInfo.kt
+++ app/src/main/java/com/kinotech/kinotechappv1/ui/profile/SubsInfo.kt
@@ -3,6 +3,5 @@ package com.kinotech.kinotechappv1.ui.profile
 data class SubsInfo (
     val uid: String = "",
     val fullName: String = "",
-    val email: String = "",
-    val profilePic: Int = 0
+    val profilePic: String = ""
 )
