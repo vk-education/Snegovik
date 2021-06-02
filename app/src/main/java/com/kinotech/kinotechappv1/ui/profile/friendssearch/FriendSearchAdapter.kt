a6ba9f4866b6af96bebc6fe08c541f8e50ae1463
Andrei Chernyshev <rei98able@yandex.ru>
2021-06-02 18:31:32 +0300

    feed last chance

--- app/src/main/java/com/kinotech/kinotechappv1/ui/profile/friendssearch/FriendSearchAdapter.kt
+++ app/src/main/java/com/kinotech/kinotechappv1/ui/profile/friendssearch/FriendSearchAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
@@ -52,6 +53,11 @@ class FriendSearchAdapter(
             binding.apply {
                 profileName.text = subsInfo.fullName.split(" ")
                     .joinToString(" ") { it.capitalize(Locale.getDefault()) }
+                Glide
+                    .with(binding.root)
+                    .load(subsInfo.photo)
+                    .error(R.drawable.ic_add)
+                    .into(binding.profilePic)
 
                 root.setOnClickListener {
                     AndroidUtils.hideKeyboard(it)
