9fc8830cde8b48127c91dc44ca51362fb832b98e
Andrei Chernyshev <rei98able@yandex.ru>
2021-06-07 23:31:36 +0300

    ktlint6

--- app/src/main/java/com/kinotech/kinotechappv1/ui/profile/ChangeProfileFragment.kt
+++ app/src/main/java/com/kinotech/kinotechappv1/ui/profile/ChangeProfileFragment.kt
@@ -61,7 +61,7 @@ class ChangeProfileFragment : Fragment() {
             checker = "clicked"
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 if (context?.let { it1 ->
-                        PermissionChecker.checkSelfPermission(
+                    PermissionChecker.checkSelfPermission(
                             it1,
                             Manifest.permission.READ_EXTERNAL_STORAGE
                         )
